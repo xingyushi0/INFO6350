44ef6b97677e5118864ddf4fc0fe1928f68e7924
xingys0 <xing.yus789@gmail.com>
2023-10-24 17:18:43 -0700

    Stock App

---
+++ StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by sylvia on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "a30c944d4d991cf11df9c50caeec78b6"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
--- StockApp/ViewController.swift
+++ StockApp/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let stockNames = ["MSFT","TSLA","META","GOOG"]
+    
+   
+    
+    
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stockNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = stockNames[indexPath.row]
+        return cell
     }
 
 
